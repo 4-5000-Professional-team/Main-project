a0cdf6bfce790ed8d1bfb95f52426293115732be
Detectiveboy <m16639649502@163.com>
2020-04-21 13:54:35 +0800

    添加备注

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
-const koa = require('koa')
-const app = new koa()
-const config = require('./config')
-const koaStatic = require('koa-static')
-const path = require('path')
-let root = path.resolve('.')
+const koa = require('koa') //引入koa框架
+const app = new koa() //对koa框架进行实例化
+const config = require('./config') //引入配置文件
+const koaStatic = require('koa-static') //引入处理静态资源的koa-static模块
+const path = require('path') //引入处理路径模块
+let root = path.resolve('.') //得到根目录
 app.use(koaStatic(path.join(root, '/static'))) //配置静态资源路径
-//静态路由配置
-const static_router = require('./app/router/static_router')
-app.use(static_router.routes()).use(static_router.allowedMethods())
-app.listen(config.port)
+const static_router = require('./app/router/static_router') //引入静态页面路由
+app.use(static_router.routes()).use(static_router.allowedMethods()) //把静态页面路由部署到服务器
+app.listen(config.port) //设置服务器端口
 console.log(`server is running at 127.0.0.1:${config.port}`)
